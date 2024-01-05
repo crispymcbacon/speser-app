748f17917a3364c6c78e16da897a04af04b1a9d2
Agustin Campagnolo <agustincampagnolo@gmail.com>
2024-01-05 15:15:50 +0100

    Update compose.yaml and server/index.js, delete logo.svg and HelloWorld.spec.js, modify api.js and main.js

--- server/index.js
+++ server/index.js
@@ -2,14 +2,14 @@ import path from 'path';
 import express from 'express';
 import router from './routes.js';
 import * as db from './db.js'
-import cors from 'cors'; // IMPORTANT: DELETE THIS IN PRODUCTION
+//import cors from 'cors'; // IMPORTANT: DELETE THIS IN PRODUCTION
 
 const __dirname = new URL(path.join(import.meta.url, '..')).pathname; // current directory
 
 // Create the server
 const app = express();
 
-app.use(cors()); // IMPORTANT: DELETE THIS IN PRODUCTION
+//app.use(cors()); // IMPORTANT: DELETE THIS IN PRODUCTION
 
 app.use(express.static(`${__dirname}/public`)); // Serve static files from the public directory
 app.use(express.json()); // Parse JSON bodies
--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import App from './App.vue'
 import router from './router'
 
 import Toast from "vue-toastification";
-// Import the CSS or use your own!
 import "vue-toastification/dist/index.css";
 
 const app = createApp(App)
